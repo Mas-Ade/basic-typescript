d0dea4c0e0e3a2ae439936486f9c2dd280add548
Mas-Ade <maulanaakbarr90@gmail.com>
2024-01-26 21:53:55 +0700

    first commit

---
+++ dist/index.js
@@ -0,0 +1,28 @@
+"use strict";
+// basic typescript
+// declare tipe data string
+const myName = "ade maulana akbar";
+// declare tipe data number default
+const myNumber = 12345;
+// declare tipe data number 
+const myNumber1 = 12345;
+// declare tipe data boolean
+const isActive = true;
+// declare tipe data array default
+const myArray = ["yaris", "fortuner"];
+// declare tipe data tuple array
+let myCar;
+myCar = ["yaris", "fortuner", 10, true];
+// declare tipe data any
+const myTool1 = 12345;
+const myTool2 = "keyboard";
+const myTool3 = true;
+const myTool4 = ["mouse", "laptop"];
+// console log
+console.log([{ myName },
+    { myNumber },
+    { isActive },
+    { myArray },
+    { myCar },
+    [myTool1, myTool2, myTool3, myTool4]
+]);
---
+++ src/index.ts
@@ -0,0 +1,36 @@
+// basic typescript
+
+// declare tipe data string
+const myName: string = "ade maulana akbar"
+
+// declare tipe data number default
+const myNumber = 12345
+
+// declare tipe data number 
+const myNumber1:number = 12345
+
+// declare tipe data boolean
+const isActive: boolean = true
+
+// declare tipe data array default
+const myArray: string[] = ["yaris", "fortuner"]
+
+// declare tipe data tuple array
+let myCar: [string, string, number, boolean]
+myCar = ["yaris", "fortuner", 10, true]
+
+// declare tipe data any
+const myTool1:any = 12345
+const myTool2:any = "keyboard"
+const myTool3:any = true
+const myTool4:any = ["mouse", "laptop"]
+
+// console log
+console.log([{myName},
+    {myNumber},
+    {isActive},
+    {myArray},
+    {myCar},
+    [myTool1, myTool2, myTool3, myTool4]
+    ])
+
